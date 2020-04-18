1d23f6c0ea9efc073b0da78e3ed86807b2a1d124
Marcia <marciaadler718@gmail.com>
2020-04-17 20:11:25 -0400

    initial folder and file structure for react app

---
+++ client/src/Pages/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <header>Community Life</header>
+    </div>
+  );
+}
---
+++ db/schema.sql
@@ -0,0 +1,4 @@
+-- Drops the blogger if it exists currently --
+DROP DATABASE IF EXISTS CommunityLife_db;
+-- Creates the "blogger" database --
+CREATE DATABASE CommunityLife_db;
