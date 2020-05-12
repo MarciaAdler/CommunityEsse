85e960829d5c5e555b28066c07ed608c75a6cf50
Marcia <marciaadler718@gmail.com>
2020-05-12 14:02:47 -0400

    fixes to upload title

--- server.js
+++ server.js
@@ -39,11 +39,11 @@ app.use(users);
 
 function getFormattedDate() {
   var today = new Date();
-  var dd = today.getDate();
-  var mm = today.getMonth() + 1;
-  var yyyy = today.getFullYear();
-  var hh = today.getHours();
-  var min = today.getMinutes();
+  var dd = today.getUTCDate();
+  var mm = today.getUTCMonth() + 1;
+  var yyyy = today.getUTCFullYear();
+  var hh = today.getUTCHours();
+  var min = today.getUTCMinutes();
   return mm + "-" + dd + "-" + yyyy + "-" + hh;
 }
 // multer instance
