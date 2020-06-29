eb91d02c05cd3bc68d71c3f85e9ed4111304494b
Marcia <marciaadler718@gmail.com>
2020-06-29 19:31:09 -0400

    added sample account login information on signin page

--- client/src/Pages/Signin.js
+++ client/src/Pages/Signin.js
@@ -20,6 +20,37 @@ export default function Signin() {
           </div>
         </Col>
       </Row>
+      <Row>
+        <Col className="col-12">
+          <div className="test-accounts mt-5">
+            <h5 className="mt-2 ml-2">
+              <strong>
+                If you are interested in testing out the app, please use the
+                test accounts listed below
+              </strong>
+            </h5>
+            <p className="ml-2">
+              <strong>Usernames:</strong>
+              <br />
+              adminHenry
+              <br />
+              HenryUser
+              <br />
+              HenryFrontDesk
+              <br />
+              MaintenanceHenry
+            </p>
+            <p className="ml-2">
+              <strong>Password for test accounts*:</strong> Test
+              <br />
+              <small>
+                *Please note, the usernames are not case sensitive, but the
+                password is
+              </small>
+            </p>
+          </div>
+        </Col>
+      </Row>
     </Container>
   );
 }
