76eec6d816b54f30f38ae2754678dc260cbb39df
Marcia <marciaadler718@gmail.com>
2020-06-29 14:25:55 -0400

    removed unused code from Files.js

--- client/src/Pages/Files.js
+++ client/src/Pages/Files.js
@@ -9,22 +9,7 @@ import LoggedOut from "../components/LoggedOut";
 
 export default function Files() {
   const [state, dispatch] = useStoreContext();
-  const [propertyName, setPropertyName] = useState(""); // storing the property name
 
-  // useEffect(() => {
-  //   if (state.currentproperty !== 0) {
-  //     getPropertyName(state.currentproperty);
-  //   } else if (state.loggedIn === true) {
-  //     getPropertyName(JSON.parse(localStorage.getItem("currentProperty")));
-  //   }
-  // }, []);
-
-  // get property name
-  // function getPropertyName(currentproperty) {
-  //   API.getPropertyName(currentproperty).then((response) => {
-  //     setPropertyName(response.data.name);
-  //   });
-  // }
   return (
     <div>
       {state.loggedIn === true ? (
