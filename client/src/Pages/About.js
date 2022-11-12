99ea33f80d27eefaf36d9358d15dcc813e8fde30
Marcia <marciaadler718@gmail.com>
2022-11-12 16:19:40 -0500

    create react app update

--- client/src/Pages/About.js
+++ client/src/Pages/About.js
@@ -1,18 +1,22 @@
 import React from "react";
-import { Container, Col } from "react-bootstrap";
+import { Container, Col, Row } from "react-bootstrap";
 
-export default function Signup() {
+export default function About() {
   return (
     <div>
       <Container className="about--container">
         <h2 className="about--title">About</h2>
-        <Col className="about--col">
-          <p>
-            If you are interested in learning more about the app or other
-            inquiries, please email Marcia at&nbsp;
-            <a href="mailto:marcia@appsbymarcia.com">marcia@appsbymarcia.com</a>
-          </p>
-        </Col>
+        <Row>
+          <Col className="about--col">
+            <p>
+              If you are interested in learning more about the app or other
+              inquiries, please email Marcia at&nbsp;
+              <a href="mailto:marcia@appsbymarcia.com">
+                marcia@appsbymarcia.com
+              </a>
+            </p>
+          </Col>
+        </Row>
       </Container>
     </div>
   );
